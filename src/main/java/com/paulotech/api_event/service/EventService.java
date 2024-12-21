1cbbde028c2a58a55b99dfcb630ecd3103b1546e
Paulo Cesar <paulocgsantos99@gmail.com>
2024-12-21 19:31:01 -0300

    feat: add db and service

---
+++ src/main/java/com/paulotech/api_event/service/EventService.java
@@ -0,0 +1,33 @@
+package com.paulotech.api_event.service;
+
+import com.paulotech.api_event.domain.event.Event;
+import com.paulotech.api_event.domain.event.EventRequestDTO;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Date;
+
+@Service
+public class EventService {
+
+    public Event createEvent(EventRequestDTO data){
+        String imgUrl = null;
+
+        if(data.image() != null){
+            imgUrl = this.uploadImg(data.image());
+        }
+
+        Event newEvent = new Event();
+        newEvent.setTitle(data.title());
+        newEvent.setDescription(data.description());
+        newEvent.setEventUrl(data.eventUrl());
+        newEvent.setDate(new Date(data.date()));
+        newEvent.setImgUrl(imgUrl);
+
+        return newEvent;
+    }
+
+    private String uploadImg(MultipartFile multipartFile){
+        return "";
+    }
+}
